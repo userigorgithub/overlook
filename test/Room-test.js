1e1aab63e0ea581c7633048ec40c8a2b1910aa98
Igor Decess <i.decess@comcast.net>
2022-04-22 14:02:40 -0600

    Add tests for implementation for function and instantiation

--- test/Room-test.js
+++ test/Room-test.js
@@ -13,4 +13,13 @@ describe('Room', () => {
 
   });
 
+  it('should be a function', () => {
+    expect(Room).to.be.a('function');
+  });
+
+  it('should be an instance of Room', () => {
+    expect(room).to.be.an.instanceOf(Room);
+  });
+
+
 });
