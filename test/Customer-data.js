7846c29884be3542c9ba3e6131655bcc652528fb
Igor Decess <i.decess@comcast.net>
2022-04-22 00:28:41 -0600

    Add customerData for test file

---
+++ test/Customer-data.js
@@ -0,0 +1,16 @@
+const customerData = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 4,
+    "name": "Kennedi Emard"
+  },
+  {
+    "id": 50,
+    "name": "Eldridge Muller"
+  }
+]
+
+export default customerData;
