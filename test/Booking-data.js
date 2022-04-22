92a8007d0bb0803d48c2412ec68f6a550c9b7d26
Igor Decess <i.decess@comcast.net>
2022-04-22 14:49:31 -0600

    Add booking data for test file

--- test/Booking-data.js
+++ test/Booking-data.js
@@ -1,1 +1,23 @@
 
+const booking = [
+  {
+    "id": "5fwrgu4i7k55hl76z",
+    "userID": 1,
+    "date": "2022/02/15",
+    "roomNumber": 4
+  },
+  {
+    "id": "5fwrgu4i7k55hl727",
+    "userID": 1,
+    "date": "2022/01/20",
+    "roomNumber": 22
+  },
+  {
+    "id": "5fwrgu4i7k55hl79t",
+    "userID": 50,
+    "date": "2022/02/04",
+    "roomNumber": 10
+  }
+]
+
+export default bookingData;
