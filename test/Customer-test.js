cdb54c0b8389e051fb51aa5ca3b0b764d0d21df9
Igor Decess <i.decess@comcast.net>
2022-04-22 00:35:42 -0600

    Create basic set up for customer testing

---
+++ test/Customer-test.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import customerData from './Customer-data.js';
+import Customer from '../src/classes/Customer';
+
+
+describe('Customer', () => {
+
+  let customer;
+
+  beforeEach(() => {
+
+  customer = new Customer(customerData);
+
+  });
+
+
+
+
+  
+});
