69076c996838cd1e929e9c3e2bf03a881db76534
Igor Decess <i.decess@comcast.net>
2022-04-25 00:04:50 -0600

    Fix apiCalls variables, imported and exported files between apiCalls and scripts, and able to connect and console.log promised data which is resolved

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,16 @@
+let apiCustomersData, apiRoomsData, apiBookingsData;
+
 const fetchData = (param) => {
   return fetch(`http://localhost:3001/api/v1/${param}`)
-    .then((response) => response.json());
+    .then((response) => response.json())
+    .then(data => console.log(data))
 };
 
 const fetchAll = () => {
-  apiUsersData = fetchData('customers');
-  apiIngredientsData = fetchData('rooms');
-  apiRecipeData = fetchData('bookings');
+  apiCustomersData = fetchData('customers');
+  apiRoomsData = fetchData('rooms');
+  apiBookingsData = fetchData('bookings');
 };
+
+
+export { fetchAll };
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,19 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import apiCalls from './apiCalls';
+import { fetchAll, apiCustomersData, apiRoomsData, apiBookingsData } from './apiCalls';
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+const loadPage = () => {
+  fetchAll();
+  Promise.all([apiCustomersData, apiRoomsData, apiBookingsData]).then((data) => testFunc(data));
+};
+
+
+window.addEventListener("load", event => loadPage());
